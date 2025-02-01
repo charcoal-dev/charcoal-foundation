8d05d6d9cc14bb6a40caf1610b63fa466c93d44c
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-01 14:32:43 +0400

    Add Problem endpoint to demonstrate error handling
    This implements a new `Problem` endpoint extending `AbstractWebEndpoint`. It intentionally triggers exceptions to test and showcase error-handling mechanisms in the web layer.

---
+++ src/interfaces/web/logic/Endpoints/Problem.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Interfaces\Web\Endpoints;
+
+use App\Interfaces\Web\AbstractWebEndpoint;
+
+/**
+ * Class Problem
+ * @package App\Interfaces\Web\Endpoints
+ */
+class Problem extends AbstractWebEndpoint
+{
+    protected function entrypoint(): void
+    {
+        throw new \RuntimeException("This exception has been intentionally triggered so you can enjoy this attractive page",
+            previous: new \LogicException("Nope! There doesn't seem to be anything wrong here."));
+    }
+}
