a129c948ac3405c854795f25e4acec5f7f39b4eb
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-07 14:08:35 +0400

    Refactor AppOrmModule to support modular components
    Introduced a new constructor and methods to handle component inclusion and database table creation dynamically. Added the ModuleComponentEnum interface to define module components. This enhances flexibility and modularity in the ORM system.

--- src/shared/Core/Orm/AppOrmModule.php
+++ src/shared/Core/Orm/AppOrmModule.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 namespace App\Shared\Core\Orm;
 
 use App\Shared\CharcoalApp;
+use App\Shared\Core\Cache\CacheStore;
+use Charcoal\App\Kernel\Build\AppBuildPartial;
 use Charcoal\App\Kernel\Orm\AbstractOrmModule;
+use Charcoal\App\Kernel\Orm\Db\DatabaseTableRegistry;
 
 /**
  * Class AppOrmModule
@@ -13,4 +16,42 @@ use Charcoal\App\Kernel\Orm\AbstractOrmModule;
  */
 abstract class AppOrmModule extends AbstractOrmModule
 {
+    private array $components;
+
+    /**
+     * @param AppBuildPartial $app
+     * @param CacheStore $cacheStore
+     * @param ModuleComponentEnum[] $components
+     */
+    protected function __construct(AppBuildPartial $app, CacheStore $cacheStore, array $components)
+    {
+        $this->components = array_unique($components);
+        parent::__construct($app, $cacheStore);
+    }
+
+    final protected function declareChildren(AppBuildPartial $app): void
+    {
+        /** @var ModuleComponentEnum $component */
+        foreach ($this->components as $component) {
+            $this->includeComponent($component, $app);
+        }
+    }
+
+    final protected function declareDatabaseTables(DatabaseTableRegistry $tables): void
+    {
+        /** @var ModuleComponentEnum $component */
+        foreach ($this->components as $component) {
+            $this->createDbTables($component, $tables);
+        }
+    }
+
+    abstract protected function includeComponent(
+        ModuleComponentEnum $component,
+        AppBuildPartial     $app
+    ): void;
+
+    abstract protected function createDbTables(
+        ModuleComponentEnum   $component,
+        DatabaseTableRegistry $tables
+    ): void;
 }
