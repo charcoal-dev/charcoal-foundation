d771cd5e646b0c54160f63d7d832946bdea03ec9
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-10 14:55:27 +0400

    Add HttpInterfaceBinding class for HTTP configuration handling
    Introduces a new class that binds HTTP interfaces to configurations using `ComponentConfigResolverTrait`. This supports dynamic and static configuration resolution based on provided parameters to streamline HTTP component integration.

---
+++ src/shared/Core/Http/HttpInterfaceBinding.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Shared\Core\Http;
+
+use App\Shared\CharcoalApp;
+use App\Shared\Core\Config\AbstractComponentConfig;
+use App\Shared\Core\Config\ComponentConfigResolverTrait;
+use App\Shared\Foundation\Http\Config\HttpInterfaceConfig;
+use App\Shared\Foundation\Http\HttpInterface;
+
+/**
+ * Class HttpInterfaceBinding
+ * @package App\Shared\Core\Http
+ */
+readonly class HttpInterfaceBinding
+{
+    public HttpInterfaceConfig $config;
+
+    use ComponentConfigResolverTrait;
+
+    /**
+     * @param CharcoalApp $app
+     * @param HttpInterface $enum
+     * @param bool $useStaticConfig
+     * @param bool $useObjectStoreConfig
+     * @param string $configClass
+     */
+    public function __construct(
+        CharcoalApp          $app,
+        public HttpInterface $enum,
+        bool                 $useStaticConfig,
+        bool                 $useObjectStoreConfig,
+        string               $configClass = HttpInterfaceConfig::class,
+    )
+    {
+        /** @noinspection PhpFieldAssignmentTypeMismatchInspection */
+        $this->config = $this->resolveConfigObject($app, $configClass, $useStaticConfig, $useObjectStoreConfig);
+    }
+
+    /**
+     * @param CharcoalApp $app
+     * @return AbstractComponentConfig|null
+     */
+    protected function resolveStaticConfig(CharcoalApp $app): ?AbstractComponentConfig
+    {
+        if (isset($app->config->http->interfaces[$this->enum->value])) {
+            return $app->config->http->interfaces[$this->enum->value];
+        }
+
+        return null;
+    }
+}
