7a68efcedb4f5d13145e6964fdf4a189a3580ad7
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-02-17 15:34:22 +0400

    Refactor ApiErrorCodeInterface method signature.
    Removed the dependency on AbstractApiEndpoint from getErrorMessage method, simplifying the interface. This change improves modularity and aligns with cleaner design principles.

--- src/shared/Core/Http/Api/ApiErrorCodeInterface.php
+++ src/shared/Core/Http/Api/ApiErrorCodeInterface.php
@@ -3,15 +3,20 @@ declare(strict_types=1);
 
 namespace App\Shared\Core\Http\Api;
 
-use App\Shared\Core\Http\AbstractApiEndpoint;
-
 /**
  * Interface ApiErrorCodeInterface
  * @package App\Shared\Core\Http\Api
  */
 interface ApiErrorCodeInterface extends \BackedEnum
 {
-    public function getErrorMessage(?AbstractApiEndpoint $context): ?string;
+    /**
+     * First argument received is always instance of AbstractApiEndpoint
+     * @return string|null
+     */
+    public function getErrorMessage(): ?string;
 
+    /**
+     * @return int|null
+     */
     public function getHttpCode(): ?int;
 }
