2c7b36854b5f65c4e0b67a37278d3335b991cd8e
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-19 17:46:04 +0500

    Refactor: Replace `Directories` with `PathRegistry`, update dependencies, improve path handling, and modernize structure

---
+++ src/shared/Core/PathRegistry.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Shared\Core;
+
+use Charcoal\Filesystem\Path\DirectoryPath;
+
+/**
+ * This class ensures that specific directories required by the application
+ * (e.g., configuration, emails, logging, temporary files, semaphores, and storage)
+ * are properly initialized and validated.
+ */
+readonly class PathRegistry extends \Charcoal\App\Kernel\Internal\PathRegistry
+{
+    public DirectoryPath $config;
+    public DirectoryPath $emails;
+    public DirectoryPath $log;
+    public DirectoryPath $semaphore;
+    public DirectoryPath $storage;
+    public DirectoryPath $tmp;
+
+    /**
+     * Declaring paths right in application constructor (no lazy loading)
+     */
+    public function declarePaths(): void
+    {
+        $this->config = $this->getValidatedPathSnapshot("/config", true, true, false, true);
+        $this->log = $this->getValidatedPathSnapshot("/log", true, true, true, true);
+        $this->tmp = $this->getValidatedPathSnapshot("/tmp", true, true, true, true);
+        $this->semaphore = $this->getValidatedPathSnapshot("/tmp/semaphore", true, true, true, true);
+        $this->storage = $this->getValidatedPathSnapshot("/storage", true, true, false, true);
+        $this->emails = $this->getValidatedPathSnapshot("/emails", true, true, false, true);
+    }
+}
