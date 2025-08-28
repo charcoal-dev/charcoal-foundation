b2c4afcbd5ff6415973a7e060cce8ecf6c31087e
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-28 06:54:34 +0500

    Add `shared` directory path to `PathRegistry` and update initialization

--- app/shared/Core/PathRegistry.php
+++ app/shared/Core/PathRegistry.php
@@ -21,6 +21,7 @@ final readonly class PathRegistry extends \Charcoal\App\Kernel\Internal\PathRegi
     public DirectoryPath $tmp;
     public DirectoryPath $log;
     public DirectoryPath $semaphore;
+    public DirectoryPath $shared;
     public DirectoryPath $templates;
     public DirectoryPath $storage;
     public DirectoryPath $emails;
@@ -33,7 +34,7 @@ final readonly class PathRegistry extends \Charcoal\App\Kernel\Internal\PathRegi
         $this->config = $this->getValidatedPathSnapshot("/config", true, true, false, true);
         $this->tmp = $this->getValidatedPathSnapshot("/tmp", true, true, true, true);
         $this->log = $this->getValidatedPathSnapshot("/log", true, true, true, true);
-        $this->semaphore = $this->getValidatedPathSnapshot("/tmp/semaphore", true, true, true, true);
+        $this->shared = $this->getValidatedPathSnapshot("/tmp/shared", true, true, true, true);
         $this->templates = $this->getValidatedPathSnapshot("/templates", true, true, true, true);
         $this->storage = $this->getValidatedPathSnapshot("/storage", true, true, false, true);
         $this->emails = $this->getValidatedPathSnapshot("/emails", true, true, false, true);
