90e14e47a9b1756d5b96efffd54b51a41003ebed
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-29 22:22:54 +0500

    Update `.dockerignore`: include `dev/bootstrap.php` necessary for Docker builds.

--- dev/bootstrap.php
+++ dev/bootstrap.php
@@ -9,26 +9,44 @@ declare(strict_types=1);
 if (defined("CHARCOAL_ROOT") ||
     defined("CHARCOAL_SAPI_ENTRYPOINT") ||
     defined("CHARCOAL_SAPI_ROOT") ||
-    defined("CHARCOAL_IN_DOCKER") ||
-    defined("CHARCOAL_SAPI_DEPTH")
+    defined("CHARCOAL_SAPI_DEPTH") ||
+    defined("CHARCOAL_IN_DOCKER")
 ) {
     throw new RuntimeException("Charcoal app bootstrap already loaded");
 }
 
+if (!getenv("CHARCOAL_SAPI_ROOT") || !getenv("CHARCOAL_SAPI_DEPTH")) {
+    throw new RuntimeException("Required CHARCOAL_SAPI_ROOT or CHARCOAL_SAPI_DEPTH not set");
+}
+
+// Get the declared directory for SAPI service
+$sapiRoot = getenv("CHARCOAL_SAPI_ROOT");
+if (!$sapiRoot || !is_dir($sapiRoot)) {
+    throw new RuntimeException("CHARCOAL_SAPI_ROOT environment variable is not set or not a directory");
+}
+
+define("CHARCOAL_SAPI_ROOT", $sapiRoot);
+
+// Get the declared depth for SAPI service
+$sapiDepth = (int)getenv("CHARCOAL_SAPI_DEPTH");
+if ($sapiDepth < 1 || $sapiDepth > 6) {
+    throw new RuntimeException("CHARCOAL_SAPI_DEPTH environment variable is not set or invalid");
+}
+
+define("CHARCOAL_SAPI_DEPTH", $sapiRoot);
+
+// Get the root directory for Charcoal App
+define("CHARCOAL_ROOT", realpath(dirname(CHARCOAL_SAPI_ROOT, $sapiDepth)));
+
+// Entrypoint
 define("CHARCOAL_SAPI_ENTRYPOINT", $_SERVER["SCRIPT_FILENAME"] ??
     debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]["file"] ??
     __FILE__);
 
-define("CHARCOAL_IN_DOCKER", @file_exists("/.dockerenv"));
-define("CHARCOAL_SAPI_DEPTH", match (basename(dirname(CHARCOAL_SAPI_ENTRYPOINT))) {
-    "engine" => CHARCOAL_IN_DOCKER ? 1 : 2,
-    "web" => CHARCOAL_IN_DOCKER ? 2 : 3,
-    default => throw new RuntimeException("Unknown SAPI"),
-});
+// Is in Docker?
+define("CHARCOAL_IN_DOCKER", defined("CHARCOAL_DOCKER") && @file_exists("/.dockerenv"));
 
-define("CHARCOAL_SAPI_ROOT", getenv("CHARCOAL_SAPI_ROOT") ?: dirname(CHARCOAL_SAPI_ENTRYPOINT));
-define("CHARCOAL_ROOT", realpath(dirname(CHARCOAL_SAPI_ROOT, CHARCOAL_SAPI_DEPTH)));
-
+// Helpers
 if (!function_exists("charcoal_from_root")) {
     function charcoal_from_root(string $path = ""): string
     {
