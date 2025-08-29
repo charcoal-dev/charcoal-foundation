c418f84d2ee5cf6881838624a3ddd0403f3b47e1
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-29 19:00:49 +0500

    Fix `build.php`: correct autoload path to proper relative directory.

--- interfaces/engine/build.php
+++ interfaces/engine/build.php
@@ -14,6 +14,7 @@ use Charcoal\Filesystem\Path\DirectoryPath;
 use Charcoal\Base\Support\Helpers\ObjectHelper;
 
 chdir(dirname(__DIR__));
+var_dump(getcwd(), __DIR__);
 require "../dev/composer/vendor/autoload.php";
 
 $stdout = new \Charcoal\Cli\Output\StdoutPrinter();
