5273eb3979d1d165548d69f20d515005cc3a29f9
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-30 08:34:04 +0500

    Update `PathRegistry` paths: move paths under `/var` for logs, temporary files, shared resources, and storage.

--- app/shared/Core/PathRegistry.php
+++ app/shared/Core/PathRegistry.php
@@ -29,10 +29,10 @@ final readonly class PathRegistry extends \Charcoal\App\Kernel\Internal\PathRegi
      */
     public function declarePaths(): void
     {
-        $this->log = $this->getValidatedPathSnapshot("/log", true, true, true, true);
+        $this->log = $this->getValidatedPathSnapshot("/var/log", true, true, true, true);
         $this->config = $this->getValidatedPathSnapshot("/config", true, true, false, true);
-        $this->tmp = $this->getValidatedPathSnapshot("/tmp", true, true, true, true);
-        $this->shared = $this->getValidatedPathSnapshot("/shared", true, true, true, true);
-        $this->storage = $this->getValidatedPathSnapshot("/storage", true, true, false, true);
+        $this->tmp = $this->getValidatedPathSnapshot("/var/tmp", true, true, true, true);
+        $this->shared = $this->getValidatedPathSnapshot("/var/shared", true, true, true, true);
+        $this->storage = $this->getValidatedPathSnapshot("/var/storage", true, true, false, true);
     }
 }
