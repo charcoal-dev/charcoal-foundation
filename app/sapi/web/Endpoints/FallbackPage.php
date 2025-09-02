2738ad1e4505d7a076d713b5b20031df7dd7d7fb
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-02 19:07:20 +0500

    Update `composer.json` to pin `http-server` dependency and annotate `FallbackPage` with `DefaultEntrypoint` attribute.

--- app/sapi/web/Endpoints/FallbackPage.php
+++ app/sapi/web/Endpoints/FallbackPage.php
@@ -9,9 +9,17 @@ declare(strict_types=1);
 namespace App\Sapi\Web\Endpoints;
 
 use Charcoal\Base\Support\Helpers\ObjectHelper;
+use Charcoal\Http\Server\Attributes\DefaultEntrypoint;
 use Charcoal\Http\Server\Contracts\Controllers\ControllerInterface;
 use Composer\InstalledVersions;
 
+/**
+ * The `FallbackPage` class is responsible for rendering a fallback template
+ * when no specific page or controller can handle the request. It gathers
+ * information such as the application's class name, the kernel build version,
+ * and the list of loaded modules to populate the fallback template.
+ */
+#[DefaultEntrypoint("entrypoint")]
 final class FallbackPage implements ControllerInterface
 {
     public function entrypoint(): void
